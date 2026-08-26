14d72c87761c2755da24cdb1e0e6373fd276a1cb
bingsingbing <279893661+inndahbing@users.noreply.github.com>
2026-08-26 22:26:09 +0700

    Add files via upload

---
+++ pertemuan_1.py
@@ -0,0 +1,28 @@
+print ("hello world")
+
+# ini komentar
+
+print("\t\tHalo Apa Kabar?");  
+print("\n Selamat datang di Lab. Teknologi Informasi");  
+print("\n Universitas Tidar");  
+
+# Menampilkan judul program
+print("\nDATA DIRI MAHASISWA")
+
+# Menampilkan nama mahasiswa
+print("\tNama \t\t\t\t\t: Indah Cahya Murti")
+
+# Menampilkan NIM mahasiswa
+print("\tNIM \t\t\t\t\t: 2605060020")
+
+# Menampilkan tempat dan tanggal lahir
+print("\tTempat, Tanggal Lahir \t: Magelang, 06 Februari 2009")
+
+# Menampilkan alamat
+print("\tAlamat \t\t\t\t\t: Magelang, Jawa Tengah")
+
+# Menampilkan jenis kelamin
+print("\tJenis Kelamin \t\t\t: Perempuan")
+
+# Menampilkan hobi
+print("\tHobi \t\t\t\t\t: Berenang dan Belajar")
